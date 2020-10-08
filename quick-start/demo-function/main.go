47015bdfb190de5bb7755e97e1516b3be1092f77
Tom Proctor <tomhjp@users.noreply.github.com>
2020-10-08 11:59:06 +0100

    Tidy logging and error handling in demo function

--- quick-start/demo-function/main.go
+++ quick-start/demo-function/main.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/hashicorp/vault/api"
@@ -18,10 +18,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var (
-	functionName = filepath.Base(os.Args[0]) // extension name has to match the filename
-	printPrefix  = fmt.Sprintf("[%s]", functionName)
-)
+const functionName = "demo-function"
 
 // Payload captures the basic payload we're sending for demonstration
 // Ex: {"payload": "hello"}
@@ -37,25 +34,25 @@ func (m Payload) String() string {
 // HandleRequest reads credentials from /tmp and uses them to query the database
 // for users. The database is determined by the DATABASE_URL environment
 // variable, and the username and password are retrieved from the secret.
-func HandleRequest(ctx context.Context, payload Payload) (string, error) {
-	println(printPrefix, "Received:", payload.String())
-	println(printPrefix, "Reading file /tmp/vault_secret.json")
+func HandleRequest(ctx context.Context, payload Payload) error {
+	logger := log.New(os.Stderr, fmt.Sprintf("[%s] ", functionName), 0)
+	logger.Println("Received:", payload.String())
+	logger.Println("Reading file /tmp/vault_secret.json")
 	secretRaw, err := ioutil.ReadFile("/tmp/vault_secret.json")
 	if err != nil {
-		println(printPrefix, fmt.Sprintf("error reading file: %w", err))
-		return "", fmt.Errorf("error reading file: %w", err)
+		return fmt.Errorf("error reading file: %w", err)
 	}
 
 	// read token
 	tokenRaw, err := ioutil.ReadFile("/tmp/vault/token")
 	if err != nil {
-		println(printPrefix, fmt.Sprintf("error reading file: %w", err))
+		return fmt.Errorf("error reading file: %w", err)
 	}
-	println(printPrefix, "raw token:", string(tokenRaw))
+	logger.Println("raw token:", string(tokenRaw))
 
 	dbURL := os.Getenv("DATABASE_URL")
 	if dbURL == "" {
-		log.Fatal("[main] no DATABASE_URL, exiting")
+		return errors.New("no DATABASE_URL, exiting")
 	}
 
 	// First decode the JSON into a map[string]interface{}
@@ -67,35 +64,35 @@ func HandleRequest(ctx context.Context, payload Payload) (string, error) {
 	dec.UseNumber()
 
 	if err := dec.Decode(&secret); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	// read users from database
 	connStr := fmt.Sprintf("postgres://%s:%s@%s/lambdadb?sslmode=disable", secret.Data["username"], secret.Data["password"], dbURL)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	var users []string
 	rows, err := db.QueryContext(ctx, "SELECT usename FROM pg_catalog.pg_user")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var user string
 		if err = rows.Scan(&user); err != nil {
-			log.Fatal(err)
+			return err
 		}
 		users = append(users, user)
 	}
-	println(printPrefix, "users: ")
+	logger.Println("users: ")
 	for i := range users {
-		println(printPrefix, "    ", users[i])
+		logger.Println("    ", users[i])
 	}
 
-	return fmt.Sprintf("Done!"), nil
+	return nil
 }
 
 func main() {
